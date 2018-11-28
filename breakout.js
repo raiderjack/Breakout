3ec1115623612d6f5844ee13b6ed59a95b8503f1
jeffreyjiang31 <44103155+jeffreyjiang31@users.noreply.github.com>
2018-11-28 14:02:05 -0800

    Update breakout.js

--- breakout.js
+++ breakout.js
@@ -5,20 +5,11 @@ ballX=500;
 ballY=650;
 
 function setup() {
+  background("black");
   createCanvas(800,700);
   k = new Ball(ballX,ballY,20,20,5,5); //make a new ball from the Ball class and call it b.
 
-  for (var row = 0; row <= 5; row++) {
-      push(); //save state of canvas
-      console.log("row " + row);
-      for (var col = 0; col <= 3; col++) {
-        rect(20, 5, 70, 30);
-        translate(75, 0); //translate in X (left-right)
-        console.log("drawing shape in row: " + row + " and column: " + col);
-      }
-      pop(); //restore state of canvas before the x translations
-      translate(0, 50); //translate in Y (down-up)
-    }
+
 
 
 }
@@ -26,13 +17,23 @@ function setup() {
 
 function draw()
 {
-	background(220);
+	background(0);
     k.drawBall();
     k.moveBall();
     k.bounceBall();
     paddle();
 
-    // brick();
+    for (var row = 0; row <= 5; row++) {
+        push(); //save state of canvas
+        console.log("row " + row);
+        for (var col = 0; col <= 9; col++) {
+          brick();
+          translate(75, 0); //translate in X (left-right)
+          console.log("drawing shape in row: " + row + " and column: " + col);
+        }
+        pop();
+        translate(0, 50);
+      }
 
 
     for (let i = 0; i < balls.length; i++) {
@@ -42,7 +43,7 @@ function draw()
             }
 }
 function paddle() {
-  stroke("black");
+  stroke("white");
   strokeWeight(10);
   line(mouseX - 40, 650, mouseX + 40, 650);
 
@@ -52,14 +53,14 @@ function keyPressed() {
     location.reload(true);
 }
 
-// function brick() {
-//   noFill();
-//   strokeWeight(1);
-//   rect(20,5,70,30);
-//
-//
-// }
-//ball class from which to create new balls with similar properties.
+function brick() {
+  fill("pink");
+  strokeWeight(1);
+  rect(20,5,70,30);
+
+
+}
+
 class Ball {
 
 	constructor(x,y,height,width,speedx,speedy) //every ball needs an x value and a y value
@@ -75,7 +76,7 @@ class Ball {
   drawBall() { // draw a ball on the screen at x,y
     stroke(0);
     strokeWeight(1);
-    fill(0);
+    fill("white");
 		ellipse(this.x,this.y,this.height,this.width);
 	  }
 	moveBall() { //update the location of the ball, so it moves across the screen
@@ -95,7 +96,6 @@ class Ball {
       fill(244,66,66);
       text("GAME OVER!",240,320)
        }
-
 
     if (this.y <= 5)
         {
