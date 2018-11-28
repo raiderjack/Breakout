575ae3bada663bc62746dcc32af9e3d104ac7e40
raiderjack <44075240+raiderjack@users.noreply.github.com>
2018-11-28 13:48:17 -0800

    Update breakout.js

--- breakout.js
+++ breakout.js
@@ -1,18 +1,26 @@
 //create a variable to hold one ball
-let b;
 let k;
 let balls = [];
 ballX=500;
 ballY=650;
-let fr=50
-
-let counter;
-let lost = false;
-
 
 function setup() {
   createCanvas(800,700);
-  k = new Ball(ballX,ballY,20,20,5,5);
+  k = new Ball(ballX,ballY,20,20,5,5); //make a new ball from the Ball class and call it b.
+
+  for (var row = 0; row <= 5; row++) {
+      push(); //save state of canvas
+      console.log("row " + row);
+      for (var col = 0; col <= 3; col++) {
+        rect(20, 5, 70, 30);
+        translate(75, 0); //translate in X (left-right)
+        console.log("drawing shape in row: " + row + " and column: " + col);
+      }
+      pop(); //restore state of canvas before the x translations
+      translate(0, 50); //translate in Y (down-up)
+    }
+
+
 }
 
 
@@ -24,13 +32,33 @@ function draw()
     k.bounceBall();
     paddle();
 
+    // brick();
+
+
     for (let i = 0; i < balls.length; i++) {
     	 	balls[i].drawBall();
            	balls[i].moveBall();
             	  balls[i].bounceBall();
             }
 }
+function paddle() {
+  stroke("black");
+  strokeWeight(10);
+  line(mouseX - 40, 650, mouseX + 40, 650);
 
+}
+function keyPressed() {
+  if (keyCode == 32)
+    location.reload(true);
+}
+
+// function brick() {
+//   noFill();
+//   strokeWeight(1);
+//   rect(20,5,70,30);
+//
+//
+// }
 //ball class from which to create new balls with similar properties.
 class Ball {
 
@@ -42,7 +70,6 @@ class Ball {
      this.width = width;
      this.speedx = speedx;
      this.speedy = speedy;
-     this.isGameOver = false;
 	  }
 
   drawBall() { // draw a ball on the screen at x,y
@@ -68,6 +95,8 @@ class Ball {
       fill(244,66,66);
       text("GAME OVER!",240,320)
        }
+
+
     if (this.y <= 5)
         {
       this.speedy = -this.speedy;
@@ -79,17 +108,6 @@ class Ball {
       console.log(this.speedy);
 
     }
-  }
-
-  function paddle() {
-    stroke("black");
-    strokeWeight(10);
-    line(mouseX - 40, 650, mouseX + 40, 650);
-
-  }
-  function keyPressed() {
-    if (keyCode == 32)
-      location.reload(true);
   }
 
 }
