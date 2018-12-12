a2822e09696d483d7f369952e2ce60deb10dcab7
jeffreyjiang31 <44103155+jeffreyjiang31@users.noreply.github.com>
2018-12-12 11:37:33 -0800

    Update breakout.js

--- breakout.js
+++ breakout.js
@@ -1,99 +1,187 @@
 //create a variable to hold one ball
-let b;
 let k;
 let balls = [];
-ballX=500;
-ballY=650;
-let fr=50
+ballX = 500;
+ballY = 650;
 
-let counter;
-let lost = false;
+brickX = 20;
+brickY = 7;
+
+bricks = [];
+
+canvasX = 1450;
+canvasY = 850;
 
 
 function setup() {
-  createCanvas(800,700);
-  k = new Ball(ballX,ballY,20,20,5,5);
-}
+  background("black");
+  createCanvas(canvasX, canvasY);
+  frameRate(180);
+  k = new Ball(ballX, ballY, 20, 20, 7, 7); //make a new ball from the Ball class and call it b.
 
 
-function draw()
-{
-	background(220);
-    k.drawBall();
-    k.moveBall();
-    k.bounceBall();
-    paddle();
-
-    for (let i = 0; i < balls.length; i++) {
-    	 	balls[i].drawBall();
-           	balls[i].moveBall();
-            	  balls[i].bounceBall();
-            }
-
-    function paddle() {
-     stroke("black");
-     strokeWeight(10);
-     line(mouseX - 40, 650, mouseX + 40, 650);
-        }
-
-
-    function keyPressed() {
-      if (keyCode == 32)
-      location.reload(true);
-        }
-}
-
-//ball class from which to create new balls with similar properties.
-class Ball {
-
-	constructor(x,y,height,width,speedx,speedy) //every ball needs an x value and a y value
-    {
-		 this.x = x;
-  	 this.y = y;
-     this.height = height;
-     this.width = width;
-     this.speedx = speedx;
-     this.speedy = speedy;
-     this.isGameOver = false;
-	  }
-
-  drawBall() { // draw a ball on the screen at x,y
-    stroke(0);
-    strokeWeight(1);
-    fill(0);
-		ellipse(this.x,this.y,this.height,this.width);
-	  }
-	moveBall() { //update the location of the ball, so it moves across the screen
-		this.x = this.x-this.speedx;
-		this.y = this.y-this.speedy;
-    }
-  bounceBall() {
-    if (this.x >= 800)
-        {
-        this.speedx = -this.speedx;
-        }
-    if (this.x <= 5)  {
-      this.speedx = -this.speedx;
-        }
-    if (this.y >= 700)  {
-      textSize(50);
-      fill(244,66,66);
-      text("GAME OVER!",240,320)
-       }
-    if (this.y <= 5)
-        {
-      this.speedy = -this.speedy;
-        }
-    if (this.x >= mouseX - 50 && this.x <= mouseX + 50 && this.y >= 650 - 12 && this.y <= 650 + 12) {
-      this.speedy = -this.speedy;
-      // this.speedx = -this.speedx;
-      console.log(this.speedx);
-      console.log(this.speedy);
+  for (let h = 0; h < 250; h += 50) {
+    for (let i = 0; i < 1400; i = i + 75) {
+      let b = new Brick(brickX + i, brickY + h, false);
+      bricks.push(b);
 
     }
   }
 
 
 
+}
+
+
+function draw() {
+  background(0);
+  k.drawBall();
+  k.moveBall();
+  k.bounceBall();
+  k.bounceBricks();
+  paddle();
+
+
+
+  for (let i = 0; i < bricks.length; i++) {
+    bricks[i].drawBrick();
+    bricks[i].breakBrick();
+
+  }
+
+}
+
+
+
+function paddle() {
+  stroke("white");
+  strokeWeight(10);
+  line(mouseX - 40, 720, mouseX + 40, 720);
+
+}
+
+function keyPressed() {
+  if (keyCode == 32)
+    location.reload(true);
+}
+
+function brick(x, y) {
+  fill("pink");
+  noStroke();
+  rect(x, y, 70, 30);
+
+
+}
+
+
+class Brick {
+
+  constructor(x, y, broke, broke1) //every ball needs an x value and a y value
+  {
+    this.x = x;
+    this.y = y;
+    this.broke = broke;
+
+  }
+
+  drawBrick() { // draw a ball on the screen at x,y
+
+    if (this.broke == true) {
+      this.x = this.x + 2000;
+      this.y = this.y + 2000;
+
+
+    } else if (this.broke == false) {
+      fill("pink");
+
+    } else {
+      fill("turquoise");
+
+    }
+    noStroke();
+    rect(this.x, this.y, 70, 30);
+  }
+
+
+  breakBrick() {
+    if (k.x >= this.x && k.x <= this.x + 70 && k.y <= this.y + 30 && k.y >= this.y) {
+      this.broke = true;
+      print(this.broke);
+
+    }
+  }
+}
+
+class Ball {
+
+  constructor(x, y, height, width, speedx, speedy) //every ball needs an x value and a y value
+  {
+    this.x = x;
+    this.y = y;
+    this.height = height;
+    this.width = width;
+    this.speedx = speedx;
+    this.speedy = speedy;
+
+  }
+
+  drawBall() { // draw a ball on the screen at x,y
+    stroke(0);
+    strokeWeight(1);
+    fill("white");
+    ellipse(this.x, this.y, this.height, this.width);
+    // balls.style.zIndex="1"
+  }
+  moveBall() { //update the location of the ball, so it moves across the screen
+    this.x = this.x - this.speedx;
+    this.y = this.y - this.speedy;
+  }
+  bounceBall() {
+    // for (let i = 0; i < bricks.length; i++){
+    //     if (this.x>= bricks[i].x && this.x <= bricks[i].x+70 && this.y <= bricks[i].y ){
+    //       this.speedy = -this.speedy;
+    //     }
+
+    if (this.x >= canvasX) {
+      this.speedx = -this.speedx;
+    }
+    if (this.x <= 35) {
+      this.speedx = -this.speedx;
+    }
+    if (this.y >= 730) {
+      textSize(50);
+      fill(244, 66, 66);
+      text("GAME OVER!", 600, 390)
+    }
+
+    if (this.y <= 5) {
+      this.speedy = -this.speedy;
+
+    }
+
+    if (this.x >= mouseX - 50 && this.x <= mouseX + 50 && this.y >= 720 - 12 && this.y <= 720 + 12) {
+      this.speedy = -this.speedy;
+      this.speedy = this.speedy * random(1, 1.03);
+      this.speedx = this.speedx * random(1, 1.03);
+      console.log("speedup");
+
+
+      console.log(this.speedx);
+      console.log(this.speedy);
+
+    }
+
+  }
+  bounceBricks() {
+    for (let i = 0; i < bricks.length; i++) {
+      if (this.x >= bricks[i].x && this.x <= bricks[i].x + 70 && this.y <= bricks[i].y + 30 && bricks[i].broke == false) {
+        this.speedy = -this.speedy;
+        frameRate = frameRate * 1.5;
+
+      }
+
+    }
+
+  }
 
 }
